3d56a2be339ef4e88c2da624189e54afc8e0d658
LasseTrs <156923845+LasseTrs@users.noreply.github.com>
2025-03-14 12:06:39 +0200

    Navigoinnin aloitus
    Signed-off-by: LasseTrs <156923845+LasseTrs@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,12 +1,84 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState, useRef } from 'react';
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import MapView, { Camera } from 'react-native-maps';
+import * as Location from 'expo-location';
+import {Marker} from 'react-native-maps';
 
 export default function App() {
+
+//https://api.open-meteo.com/v1/forecast?latitude=65.01&longitude=65.01&hourly=temperature_2m,weather_code&forecast_days=1
+//säätiedot tuolta
+const [camera, setCamera] = useState('')
+    const [location, setLocation] = useState({
+        latitude: 65.0100,
+        longitude: 65.0100,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      })
+      
+      useEffect(() => {
+      (async() =>{
+        getUserPosition()
+      })()
+        
+      }, [])
+
+    const getUserPosition = async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        try {
+          if (status !== 'granted') {
+            console.log('Permission denied');
+            return;
+          }
+      
+          const position = await Location.getCurrentPositionAsync({
+            accuracy: Location.Accuracy.High,
+          });
+          setLocation({
+            ...location,
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+          setCamera({
+...camera,
+pitch: 90,
+heading: 0,
+          zoom: 20,
+
+          })
+ 
+        } catch (error) {
+          console.log(error);
+        }
+      };
+ 
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+       <SafeAreaView>
+<MapView
+        style={styles.map}
+        mapType="hybrid"
+        camera={{
+          center: {
+            latitude: 65.010,
+            longitude: 65.010,
+          },
+          pitch: 90, 
+          heading: 0,
+          zoom: 15, 
+        }}
+        showsUserLocation={true}
+        followUserLocation={true}
+        showsCompass={false}
+        showsBuildings={true}
+        pitchEnabled={false}
+      />
+      
+</SafeAreaView>
+
+    
   );
 }
 
@@ -16,5 +88,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }, map: {
+    height: '100%',
+    width: '100%'
+  }
 });
